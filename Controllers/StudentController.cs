74c151197d6c49c300397ae2381a0e8f4fc7a8d5
MarkNovak <novakovmark21@gmail.com>
2024-11-29 20:34:28 +0100

    Initial commit #5

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -5,9 +5,12 @@ namespace Studentko.Controllers;
 
 public class StudentController: Controller{
     [HttpPost]
-    public IActionResult Login(string usr, string pwd){
+    public IActionResult Login(string username, string pwd){
               System.Console.WriteLine("halo tukaj nek bakcend naredi");
               return Content("Logiral si se, sam nimamo baze :(");
     }
-
+    public IActionResult Register(string username, string pwd, string email){
+        System.Console.WriteLine(username);
+        return Content($"Račun {username} je bil uspešno narejen");       
+    }    
 }
