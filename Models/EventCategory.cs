4eb16fdaa2d054712ddaa1cf0d3eb8bc9429689f
Jernej Rigler <121813160+JernejRigler@users.noreply.github.com>
2024-12-04 13:31:33 +0100

    Create EventCategory.cs

---
+++ Models/EventCategory.cs
@@ -0,0 +1,9 @@
+namespace Studentko.Models;
+using System;
+using System.Collections.Generic;
+public class Category
+{
+    public int ID { get; set; }
+    public string category { get; set; }
+
+}
