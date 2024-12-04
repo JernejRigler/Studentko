e728cd135b3558857e0841a9a4c71af56006d5e0
Jernej Rigler <121813160+JernejRigler@users.noreply.github.com>
2024-12-04 13:28:11 +0100

    Update Post.cs

--- Models/Post.cs
+++ Models/Post.cs
@@ -7,5 +7,6 @@ public class Post
     public string subtitle { get; set; }
     public string content { get; set; }
     public string fileAttachment { get; set; }
+    public bool isTrending { get; set; } 
 
 }
