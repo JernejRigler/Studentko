d1d0839689e59a389363f899c602197f45543a25
Jernej Rigler <121813160+JernejRigler@users.noreply.github.com>
2024-12-04 13:26:56 +0100

    Create Post.cs

---
+++ Models/Post.cs
@@ -0,0 +1,11 @@
+namespace Studentko.Models;
+using System;
+using System.Collections.Generic;
+public class Post
+{
+    public string title { get; set; }
+    public string subtitle { get; set; }
+    public string content { get; set; }
+    public string fileAttachment { get; set; }
+
+}
