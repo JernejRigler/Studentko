1b0496568762724348a913c4c7900494fb23fe38
Jernej Rigler <121813160+JernejRigler@users.noreply.github.com>
2024-12-04 13:35:42 +0100

    Update StudentkoContext.cs

--- Data/StudentkoContext.cs
+++ Data/StudentkoContext.cs
@@ -11,11 +11,17 @@ public class StudentkoContext : IdentityDbContext<ApplicationUser>
     public StudentkoContext(DbContextOptions<StudentkoContext> options) : base(options) { }
 
     //public required DbSet<ApplicationUser> User { get; set; }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<EventCategory> EventCategories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<ApplicationUser>().ToTable("User");
+        modelBuilder.Entity<Event>().ToTable("Event");
+        modelBuilder.Entity<Post>().ToTable("Post");
+        modelBuilder.Entity<EventCategory>().ToTable("EventCategory");
 
     }
 
