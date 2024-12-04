2f342dbfdbb078afe5e2fd7d94d8580bb7357d93
Jernej Rigler <121813160+JernejRigler@users.noreply.github.com>
2024-12-04 13:30:18 +0100

    Update Event.cs

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,1 +1,11 @@
+namespace Studentko.Models;
+using System;
+using System.Collections.Generic;
+public class Event
+{
+    public string title { get; set; }
+    public int numberSpaces { get; set; }
+    public string description { get; set; }
+    public bool isTrending { get; set; } 
 
+}
