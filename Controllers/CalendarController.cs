6cfc1b2ccf3c703f355b58adfaae8085f99dbdaf
MarkNovak <novakovmark21@gmail.com>
2025-01-04 20:39:25 +0100

    Added event displaying on calendar

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -1,17 +1,27 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Studentko.Models;
 using Studentko.Data;
-
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace Studentko.Controllers;
 
 public class CalendarController : Controller
 {
-    public IActionResult ViewCalendar()
+    private readonly StudentkoContext _context;
+     public CalendarController(StudentkoContext context)
     {
+        _context = context;
+    }
+
+    public async  Task<IActionResult> ViewCalendar()
+    {
+        var eventDates = await _context.Event.ToListAsync();
+
         Console.WriteLine("Halo želiš videti koledar jaaja");
-        return View();
+        return View(eventDates);
     }
 
 }
